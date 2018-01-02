b71ef2b477353d006c390b0825a2b0b2bfeb718e
Tony Pai <tony77794@gmail.com>
2018-01-02 17:18:43 +0800

    Add react-app repo
    * Use create-react-app CLI to create client side app

---
+++ packages/react-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { AlertButton, ToggleButton } from 'shared-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <h2>Shared Buttons:</h2>
+        <AlertButton />&nbsp;
+        <ToggleButton />
+      </div>
+    );
+  }
+}
+
+export default App;
