1019138608fca1196cf18c163b1a2f67ed03886c
Tony Pai <tony77794@gmail.com>
2021-01-04 20:43:26 +0800

    Upgrade react-app

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { AlertButton, ToggleButton } from 'shared-components';
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h2>Shared Buttons</h2>
-        <AlertButton />&nbsp;
-        <ToggleButton />
-      </div>
-    );
-  }
+import { AlertButton, ToggleButton } from "shared-components";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>Shared Components</h1>
+      <AlertButton />
+      &nbsp;
+      <ToggleButton />
+    </div>
+  );
 }
 
 export default App;
